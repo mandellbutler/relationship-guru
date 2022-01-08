5583651f8c937103fbaa26a9f74a97f758f1bd1e
Mandell Butler <mandell@mandells-mbp.lan>
2022-01-07 20:21:18 -0500

    added title element to wireframe

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const header = document.getElementById("site-header");
+const main = document.getElementById("site-main");
 
 
 //Create Elements
 const navBar = document.createElement("nav");
+const titleElement = document.createElement("h1");
 
 
 navBar.setAttribute("id", "navBar");
+titleElement.setAttribute("id", "siteTitle");
+
 
 
 
@@ -16,7 +20,9 @@ navBar.innerHTML = `
 <li>Contact Us</li>
 </ul>
 `;
+titleElement.innerHTML = "Hello World!";
 
 
 
 header.append(navBar);
+main.append(titleElement);
