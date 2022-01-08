5ef9f5ba542594764aaafcd77374ff4190c93f82
Mandell Butler <mandell@mandells-mbp.lan>
2022-01-07 20:23:52 -0500

    added article element and psuedo content to wireframe

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const main = document.getElementById("site-main");
 //Create Elements
 const navBar = document.createElement("nav");
 const titleElement = document.createElement("h1");
+const article = document.createElement("article");
 
 
 navBar.setAttribute("id", "navBar");
 titleElement.setAttribute("id", "siteTitle");
+article.setAttribute("id", "article-content")
 
 
 
@@ -21,8 +23,28 @@ navBar.innerHTML = `
 </ul>
 `;
 titleElement.innerHTML = "Hello World!";
+article.innerHTML = `
+<ul><p>Article Area</p>
+<li>Featured Article
+<h2>Title</h2>
+<p>orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.<p>
+</li>
+<li>Archive:
+<h2>Title</h2>
+<p>orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.
+orenLorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quas. Neque architecto vel esse odit, labore suscipit dicta exercitationem ullam repellendus aliquam at autem cumque saepe aperiam dolore ut necessitatibus.<p>
+</li>
+<li>Another Placeholder</li>
+</ul>
+`
 
 
 
 header.append(navBar);
 main.append(titleElement);
+main.append(article);
