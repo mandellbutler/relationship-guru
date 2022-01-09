c48b31968dabbbb20f87eb89570358a2438d3a02
Mandell Butler <mandell@mandells-mbp.lan>
2022-01-08 23:31:36 -0500

    created contact class constructor

---
+++ components/contact.js
@@ -0,0 +1,26 @@
+class Contact {
+    constructor (
+        id,
+        firstName,
+        lastName,
+        relation,
+        birthdate,
+        age,
+        favoriteColor,
+        giftIdeas,
+        dateIdeas,
+        image
+    )
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.relation = relation;
+        this.birthdate = birthdate;
+        this.age = age;
+        this.favoriteColor = favoriteColor;
+        this.giftIdeas = giftIdeas;
+        this.dateIdeas = dateIdeas;
+        this.image = image;
+    }
+}
