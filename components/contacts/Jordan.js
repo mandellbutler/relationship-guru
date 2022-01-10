f7c2f00c1df272ff314d5c5f47d5d7e26ee85d42
Mandell Butler <mandell.butler@outlook.com>
2022-01-10 15:37:08 -0500

    built jordan instance of contact class

---
+++ components/contacts/Jordan.js
@@ -0,0 +1,19 @@
+import Contact from "../contact.js"
+
+const Jordan = new Contact (
+    1,
+    "jordan",
+    "jacobs",
+    "Brother",
+    "January 23, 1993",
+    "",
+    "Black",
+    ["Headphones", "Gucci Socks", "Handguns", "Pokemon"],
+    [],
+    {
+        "src": "../assets/images/contact.jpg",
+        "alt": "=graphic image of annonymous user"
+    }
+)
+
+export default Jordan;
