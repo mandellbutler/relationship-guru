c0895ba36afcd5ddc4f14f4de809e7bbacdeeb09
Mandell Butler <mandell.butler@outlook.com>
2022-01-12 16:02:02 -0500

    built Kyle instance of contact class

---
+++ components/contacts/Kyle.js
@@ -0,0 +1,22 @@
+import Contact from "../contact.js";
+
+const Kyle = new Contact (
+    2,
+    "kyle",
+    "joseph",
+    "Cousin",
+    "February 15, 1983",
+    "",
+    "Powder Blue",
+    ["Dinner Set", "Jogging Suit", "Art Supplies", "Tennis Balls"],
+    [],
+    {
+        "src": "src=../assets/images/contact.jpg",
+        "alt": "alt=graphic image of annonymous user"
+    }
+)
+
+Kyle.buildContactElement();
+Kyle.announceEvent();
+
+export default Kyle;
