842a311b582169e354cb458ed99eff1d4693b1b9
Mandell Butler <mandell.butler@outlook.com>
2022-01-14 07:23:27 -0500

    added geene instance to contact class

---
+++ components/contacts/Geene.js
@@ -0,0 +1,22 @@
+import Contact from "../contact.js"
+
+const Geene = new Contact (
+    3,
+    "geene",
+    "banks",
+    "Agent",
+    "March 23, 1957",
+    "",
+    "Blue",
+    ["Books", "Cologne", "Kitchenware", "Football Cards"],
+    [],
+    {
+        "src": "src=../assets/images/contact.jpg",
+        "alt": "alt=graphic image of annonymous user"
+    }
+)
+
+Geene.buildContactElement();
+Geene.announceEvent();
+
+export default Geene;
